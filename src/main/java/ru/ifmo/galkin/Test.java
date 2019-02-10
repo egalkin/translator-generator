eb83083684be9c1febc84f081bda9bb7a2f7d929
Egor Galkin <egor.galkin99@yandex.ru>
2019-02-11 00:00:21 +0300

    Add tests

--- src/main/java/ru/ifmo/galkin/Test.java
+++ src/main/java/ru/ifmo/galkin/Test.java
@@ -15,9 +15,14 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String... args) throws ParseException, IOException {
 //        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(new ByteArrayInputStream("1+2".getBytes()));
-        Parser parser = new Parser(new ByteArrayInputStream("(1+2*4)*4/3".getBytes()));
-        Tree res = parser.expr();
-        System.out.println(res.getValue());
+//        Parser parser = new Parser(new ByteArrayInputStream("(1+2*4)*4/3".getBytes()));
+//        Parser parser = new Parser(new ByteArrayInputStream("((1+2*4)*4/3".getBytes()));
+//        Tree res = parser.expr();
+//        System.out.println(res.getValue());
+//        TreeWriter.treeToDotCode(res);
+        Parser parser = new Parser(new ByteArrayInputStream("(())".getBytes()));
+        Tree res = parser.s();
+        System.out.println(parser.isFinished());
         TreeWriter.treeToDotCode(res);
 //        lexicalAnalyzer.nextToken();
 //        while (lexicalAnalyzer.getCurToken() != Token.END) {
