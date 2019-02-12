0358413995dd1023b3dae02e6266b7d52df7e246
Egor Galkin <egor.galkin99@yandex.ru>
2019-02-12 23:55:07 +0300

    Update Test class

--- src/main/java/ru/ifmo/galkin/Test.java
+++ src/main/java/ru/ifmo/galkin/Test.java
@@ -4,6 +4,7 @@ import ru.ifmo.galkin.gparse.LexicalAnalyzer;
 import ru.ifmo.galkin.gparse.Parser;
 import ru.ifmo.galkin.gparse.Token;
 import ru.ifmo.galkin.gparse.Tree;
+import ru.ifmo.galkin.grammar.Terminal;
 
 
 import java.io.ByteArrayInputStream;
@@ -11,23 +12,14 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Test {
-    public static void main(String... args) throws ParseException, IOException {
-//        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(new ByteArrayInputStream("1+2".getBytes()));
-//        Parser parser = new Parser(new ByteArrayInputStream("(1+2*4)*4/3".getBytes()));
-//        Parser parser = new Parser(new ByteArrayInputStream("((1+2*4)*4/3".getBytes()));
-//        Tree res = parser.expr();
-//        System.out.println(res.getValue());
-//        TreeWriter.treeToDotCode(res);
-        Parser parser = new Parser(new ByteArrayInputStream("(())".getBytes()));
-        Tree res = parser.s();
-        System.out.println(parser.isFinished());
-        TreeWriter.treeToDotCode(res);
-//        lexicalAnalyzer.nextToken();
-//        while (lexicalAnalyzer.getCurToken() != Token.END) {
-//            System.out.println(lexicalAnalyzer.getCurTokenString());
-//            lexicalAnalyzer.nextToken();
-//        }
+    public static void main(String... args) throws ParseException {
+        Parser parser = new Parser(new ByteArrayInputStream("16 + 32 * 44 + 55/5 - 14".getBytes()));
+        Tree res = parser.parse();
+        System.out.println(res.getValue());
     }
 }
